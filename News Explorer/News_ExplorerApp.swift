c39f6707704d7cec9bc16d92ea155fe5ea8626b9
Ivan Maister <ivanmaister7@gmail.com>
2023-10-06 10:40:58 +0300

    Initial Commit

---
+++ News Explorer/News_ExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  News_ExplorerApp.swift
+//  News Explorer
+//
+//  Created by Master on 06.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct News_ExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
